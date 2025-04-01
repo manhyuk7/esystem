86120e3f467ce5736a8c63c2d2cae8b1c3a2dc77
manhyuk7 <115455983+manhyuk7@users.noreply.github.com>
2025-04-01 14:43:12 +0900

    use the gpio ioremap & GPIO 25 interrupt

--- switch_drv/switch_drv.c
+++ switch_drv/switch_drv.c
@@ -13,11 +13,25 @@
 #include <linux/interrupt.h>
 
 /* BCM2711 CPU */
-#define MAJOR_NUMBER   102
-#define DEVICE_NAME      "switch_dd"
-#define GPIO_LED1      18 /* LED1 gpio number */
-#define GPIO_LED2      23 /* LED2 gpio number */
+#define DEVICE_NAME "switch_dd"  /* mknod /dev/led_dd c 102 0 */
+#define MAJOR_NUMBER   102  /* device number : major */
+
+#define GPIO_BASE 0xFE200000    /* 라즈베리파이 4의 GPIO 베이스 주소 */
+
+#define GPIO_SET_OFFSET_18 0x1C    /* LED 18 : SET Offset */
+#define GPIO_CLR_OFFSET_18 0x28    /* LED 18:  Clear Offset */
+#define GPIO_SEL_OFFSET_18 0x04    /* LED 18:  Selection Offset */
+
+#define GPIO_SET_OFFSET_23 0x1C    /* LED 23 : SET Offset */
+#define GPIO_CLR_OFFSET_23 0x28    /* LED 23:  Clear Offset */
+#define GPIO_SEL_OFFSET_23 0x08    /* LED 23:  Selection Offset */
+
+#define GPIO_SEL_OFFSET_25 0x08    /* LED 25 : SET Offset */
+
 #define GPIO_SW         25 /* Switch gpio number */
+
+volatile void __iomem *gpio_base;
+unsigned int sel_val;
 
 static int switch_irq;     /* switch gpio's IRQ number */
 static int irq_count = 0;  /* interrupt count */
@@ -26,20 +40,24 @@ static char irq_info[2];   /* interrupt info for returnning to app */
 /* gpio ISR : interrupt service routine */
 static irqreturn_t isr_func(int irq, void *dev){
 
-    static int gpio_value;
-	irq_count++; /* interrupt count ++ */
-
-    gpio_value = gpio_get_value(GPIO_LED1); /* GPIO_LED1 value */
+    irq_count++; /* interrupt count ++ */
 
     printk("Switch driver: isr total count = %d\n", irq_count);
     printk("Switch driver: isr irq number  = %d\n", irq);
-    printk("Switch driver: isr parameter   = %s\n", dev);
-    printk("Switch driver: gpio_get_value  = %d\n", gpio_value);
- 
-    gpio_set_value(GPIO_LED1, !gpio_value);
-    gpio_set_value(GPIO_LED2, !gpio_value);
+    printk("Switch driver: isr parameterr  = %s\n", dev);
 
-    return IRQ_HANDLED;
+   if(irq_count % 2 == 0) {
+    // GPIO 18/23: LED 끄기
+        writel(1 << 18, gpio_base + GPIO_CLR_OFFSET_18);
+        writel(1 << 23, gpio_base + GPIO_CLR_OFFSET_23);
+   }
+   else {
+    // GPIO18/23: LED 켜기
+        writel(1 << 18, gpio_base + GPIO_SET_OFFSET_18);
+        writel(1 << 23, gpio_base + GPIO_SET_OFFSET_23);
+   }   
+
+        return IRQ_HANDLED;
 }
 
 /* device driver open */
@@ -81,8 +99,9 @@ static struct file_operations gpio_fops = {
    .unlocked_ioctl = switch_ioctl
 };
 
-/* $sudo mknod /dev/button_dd 255 0 */
-/* $sudo chmod 666 /dev/button_dd */
+/* $sudo mknod /dev/switch_dd 102 0 */
+/* $sudo chmod 666 /dev/switch_dd */
+
 static int switch_init(void) {
     int err, i;
 
@@ -93,24 +112,48 @@ static int switch_init(void) {
 	return MAJOR_NUMBER;
     }
 	
+    /* gpio memory mapping */
+    gpio_base = ioremap(GPIO_BASE, 0x100);
+    if (!gpio_base) {
+        pr_err("Failed to map GPIO memory\n");
+        return -ENOMEM;
+    }
+
     /* gpio register */
-    gpio_request(GPIO_LED1, "LED");      /* GPIO allocation */
-    gpio_direction_output(GPIO_LED1, 0); /* GPIO set as output */
-    gpio_request(GPIO_LED2, "LED");      /* GPIO GPIO allocation */
-    gpio_direction_output(GPIO_LED2, 0); /* GPIO set as output */
+    // GPIO 핀 설정 (예: GPIO 18을 출력으로 설정)
+    sel_val = readl(gpio_base + GPIO_SEL_OFFSET_18);
+    sel_val &= ~(7 << 24);            // GPIO 18 위치 초기화
+    sel_val |= (1 << 24);                // GPIO 18을 출력으로 설정
+    writel(sel_val, gpio_base + GPIO_SEL_OFFSET_18);
+
+  // GPIO 핀 설정 (예: GPIO 23을 출력으로 설정)
+    sel_val = readl(gpio_base + GPIO_SEL_OFFSET_23);
+    sel_val &= ~(7 << 9);            // GPIO 23 위치 초기화
+    sel_val |= (1 << 9);                // GPIO 23 을 출력으로 설정
+    writel(sel_val, gpio_base + GPIO_SEL_OFFSET_23);
+
+  // GPIO 핀 설정 (예: GPIO 25을 입력으로 설정)
+    sel_val = readl(gpio_base + GPIO_SEL_OFFSET_25);
+    sel_val &= ~(7 << 15);            // GPIO 25 위치 초기화
+    sel_val |= (0 << 15);                // GPIO 25 을 출력으로 설정
+    writel(sel_val, gpio_base + GPIO_SEL_OFFSET_25);
 
     for(i = 0; i < 5; i++) {
-        gpio_set_value(GPIO_LED1, 1);
-        gpio_set_value(GPIO_LED2, 1);
-        mdelay(500);
-	gpio_set_value(GPIO_LED1, 0);
-	gpio_set_value(GPIO_LED2, 0);
-        mdelay(500);
+    // GPIO18/23: LED 켜기
+        writel(1 << 18, gpio_base + GPIO_SET_OFFSET_18);
+        writel(1 << 23, gpio_base + GPIO_SET_OFFSET_23);
+        mdelay(1000); 
+
+    // GPIO 18/23: LED 끄기
+        writel(1 << 18, gpio_base + GPIO_CLR_OFFSET_18);
+        writel(1 << 23, gpio_base + GPIO_CLR_OFFSET_23);
+        mdelay(1000);
     }
 	
     /* register interrupt handler */
-    gpio_request(GPIO_SW, "SWITCH"); /* GPIO allocation */
-    switch_irq = gpio_to_irq(GPIO_SW); /* return irq number. GPIO irq happens when gpio input change */
+      switch_irq = gpio_to_irq(GPIO_SW); /* return irq number. GPIO irq happens when gpio input change */
+                                                     /* 라즈베리파이4는 GPIO 컨트롤러와 IRQ 컨트롤러가 매핑되어 있음 */
+                                                     /*  /proc/interrupt: 57:          3          0          0          0  pinctrl-bcm2835  25 Edge */
     /* register interrupt service*/ 
     err = request_irq(switch_irq, isr_func, IRQF_TRIGGER_RISING, "switch", "GOOD");
 
@@ -125,13 +168,14 @@ static int switch_init(void) {
 
 static void switch_exit(void){
 
-    unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME); /* /dev/button_dd unregister */
-	
-    /* Interrupt ?´ì œ */
+    unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME); /* /dev/switch_dd unregister */
+
+    /* Interrupt release */
     free_irq(switch_irq, "GOOD");
-    gpio_free(GPIO_SW);
-    gpio_free(GPIO_LED1);
-    gpio_free(GPIO_LED2);
+
+   // 메모리 매핑 해제
+    if (gpio_base)
+        iounmap(gpio_base);
 
     printk(KERN_INFO "Switch driver: released!\n");
 }
